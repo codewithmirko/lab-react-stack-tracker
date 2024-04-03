3f861a8b3c4c3b5bd6ab8d958e9bd116cd32d26e
Mirko Schaefer <codewithmirko@gmail.com>
2024-04-03 15:28:33 +0200

    Solved until interation 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import companiesData from "./companies.json";
+import HomePage from "./pages/HomePage";
+import technologiesData from "./technologies.json";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import TechnologyPage from "./pages/TechnologyPage";
+import CompanyPage from "./pages/CompanyPage";
 
 function App() {
+  const [companies, setCompanies] = useState(companiesData);
+  // const [technologies, setTechnologies] = useState(technologiesData);
+
+  console.log(companiesData);
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} companies={companies} />
+        <Route path="company/:companySlug" element={<CompanyPage />} />
+        <Route path="/tech/:slug" element={<TechnologyPage />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
-function HomePage() {
+function HomePage({ companies }) {
+  console.log({ companies });
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      {companies &&
+        companies.map((currentCompany) => {
+          return (
+            <div key={currentCompany.id}>
+              <h1>{currentCompany.name}</h1>
+              <img src="#" alt="" />
+            </div>
+          );
+        })}
     </div>
   );
 }
